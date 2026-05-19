1aed826c1f7db8a97b7cbedea4355a0bb7914a07
Soumya <nalinisahoo781@gmail.com>
2026-05-19 22:21:45 +0530

    fixed projectcard scroll type added parallax effect using gsap

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useRef } from 'react'
 import cashflow from '../assets/cashflow.png'
 import animation from '../assets/animation.png'
 import netflix from '../assets/netflix.png'
@@ -9,10 +9,52 @@ import { FaCss3Alt, FaGithub, FaHtml5, FaNodeJs, FaReact } from 'react-icons/fa'
 import { RiTailwindCssFill } from 'react-icons/ri'
 import { IoLogoJavascript } from 'react-icons/io'
 import { SiExpress, SiMongodb, SiRedux } from 'react-icons/si'
+import { useGSAP } from '@gsap/react'
+import gsap from 'gsap'
 
 const ProjectCard = ({ onButtonClick }) => {
 
-    const [hoveredId, setHoveredId] = useState(null);
+    const wrapperRef = useRef(null);
+    const contentRef = useRef(null);
+
+    useGSAP(() => {
+
+        () => {
+
+            ScrollSmoother.create({
+
+                wrapper: wrapperRef.current,
+                content: contentRef.current,
+                smooth: 1.2,
+                effects: true,
+                smoothTouch: 0.1,
+
+            });
+
+        }
+
+        const cards = gsap.utils.toArray('.card');
+
+        cards.forEach((card, i) => {
+
+            gsap.to(card, {
+                scale: 0.8 + 0.2 * (i / 3),
+                ease: "none",
+
+                scrollTrigger: {
+                    trigger: card,
+                    start: `top ${100 + 35 * i}`,
+                    end: "bottom bottom",
+                    endTrigger: ".container",
+                    scrub: true,
+                    pin: card,
+                    pinSpacing: false,
+                    invalidateOnRefresh: true,
+                },
+            });
+        });
+
+    }, []);
 
     const projectPreview = [
         {
@@ -125,128 +167,135 @@ const ProjectCard = ({ onButtonClick }) => {
     // }
 
     return (
-        <div className="flex flex-col items-center relative bg-gray-300 px-0 md:px-5 z-20">
-            {projectPreview.map((items) => {
-                const { id, title, description, techstack, image, time } = items;
+        <div className='relative bg-gray-300 px-0 md:px-5 z-20 flex items-center justify-center'
+            ref={wrapperRef}
+        >
+            <div className="flex container flex-col items-center justify-center md:mt-20 mt-10"
+                ref={contentRef}
+            >
+                {projectPreview.map((items) => {
+                    const { id, title, description, techstack, image, time } = items;
 
-                return (
-                    <div
-                        key={id}
-                        className="w-full lg:w-[90vw] h-auto mb-5 sm:mb-8 md:mb-0 sticky top-4 md:top-0 px-2 sm:px-4 py-3 sm:py-5 md:py-10 bg-transparent"
-                    >
-
+                    return (
                         <div
-                            className={`hidden md:flex gap-5 w-fit max-w-[30vw] bg-slate-900 justify-center items-center rounded-lg transition-all duration-700 px-5 py-3 border-0 sm:border border-gray-300
-                            ${hoveredId === id ? "ml-[65%] opacity-100" : "ml-0 opacity-0"}`}
-                        >
-                            <p className="text-white flex gap-2"><span><StickyNote /></span>0{id}/3</p>
-                            <p className="text-white">{title}</p>
-                        </div>
-
-                        <div
-                            className={`flex flex-col md:flex-row justify-between gap-4 sm:gap-6     md:gap-8 mt-3 sm:mt-4 md:mt-5
-                             px-4 sm:px-6 md:px-10 py-5 sm:py-6 md:py-10
-                            bg-slate-900 items-center border-2 border-gray-300 rounded-lg overflow-hidden
-                            transition-all duration-700
-                                ${hoveredId === id
-                                    ? "md:scale-[1.03] md:shadow-2xl md:shadow-black/60 md:transform-[perspective(1000px)_rotateX(-3deg)]"
-                                    : "scale-100"
-                                }`}
-                            style={{ transformStyle: "preserve-3d" }}
-                            onMouseEnter={() => setHoveredId(id)}
-                            onMouseLeave={() => setHoveredId(null)}
+                            key={id}
+                            className="w-full lg:w-[90vw] h-auto mb-5 sm:mb-8 md:mb-0  px-2 sm:px-4 py-3 sm:py-5 md:py-10 flex items-center justify-center"
                         >
 
-                            <div className="w-full md:w-1/2 lg:w-[40vw] h-48 sm:h-64 md:h-72 lg:h-[25vw] rounded-lg overflow-hidden shrink-0">
-                                <img
-                                    src={image}
-                                    loading="lazy"
-                                    className="rounded-lg object-cover w-full h-full hover:scale-105 hover:shadow-[0_0_20px_rgba(255,255,255,0.1)] transition-all duration-700 border-2 border-gray-800"
-                                />
-                            </div>
+                            {/* <div
+                                className='hidden md:flex gap-5 w-fit max-w-[30vw] bg-slate-900 justify-center items-center rounded-lg transition-all duration-700 px-5 py-3 border-0 sm:border border-gray-300'
+                            >
+                                <p className="text-white flex gap-2"><span><StickyNote /></span>0{id}/3</p>
+                                <p className="text-white">{title}</p>
+                            </div> */}
 
-                            <div className="w-full flex flex-col items-center">
+                            <div
+                                className='card flex flex-col md:flex-row justify-between
+                            px-4 sm:px-6 md:px-10 py-5 sm:py-6 md:py-10
+                            bg-slate-900 items-center border-2 border-gray-300 rounded-lg overflow-hidden gap-10 shadow-md shadow-gray-500'
+                            >
 
-                                <div className="text-white w-full md:mt-10">
-                                    <SplitText
-                                        text={title}
-                                        className="text-base sm:text-lg md:text-2xl w-full font-semibold"
-                                        delay={500} duration={1.9} ease="power3.out"
-                                        splitType="words"
-                                        from={{ opacity: 0, y: 40 }} to={{ opacity: 1, y: 0 }}
-                                        threshold={0.1} rootMargin="-100px" showCallback={false}
+                                <div className="w-full md:w-1/2 lg:w-[40vw] h-48 sm:h-64 md:h-72 lg:h-[25vw] rounded-lg overflow-hidden shrink-0">
+                                    <img
+                                        src={image}
+                                        loading="lazy"
+                                        className="rounded-lg object-cover w-full h-full hover:scale-105 hover:shadow-[0_0_20px_rgba(255,255,255,0.1)] transition-all duration-700 border-2 border-gray-800"
                                     />
                                 </div>
 
-                                <div className="hidden sm:block h-px w-full bg-gray-600 rounded-lg mt-3" />
+                                <div className="w-full flex flex-col items-center">
 
-                                <div className="hidden sm:block w-full mt-4 md:mt-5 text-gray-400">
-                                    <SplitText
-                                        text={description}
-                                        className="text-sm md:text-base font-semibold"
-                                        delay={10} duration={1.9} ease="power3.out"
-                                        splitType="words"
-                                        from={{ opacity: 0, y: 40 }} to={{ opacity: 1, y: 0 }}
-                                        threshold={0.1} rootMargin="-100px" showCallback={false}
-                                    />
-                                </div>
-
-                                <div className="grid grid-cols-4 lg:grid-cols-4 gap-2 sm:gap-3 mt-3 sm:mt-5 w-full">
-                                    {techstack.map((stacks, index) => (
-                                        <button
-                                            key={index}
-                                            className="flex items-center justify-center gap-1.5 py-2 rounded-md bg-white/5 backdrop-blur-md text-white text-[8px] sm:text-xs lg:text-sm transition-all duration-300 hover:bg-white/10 hover:border-white/40 hover:scale-[1.03] active:scale-[0.97]"
-                                        >
-                                            <span>{stacks.logo}</span>
-                                            {stacks.name}
-                                        </button>
-                                    ))}
-                                </div>
-
-                                <div className="hidden sm:block h-px w-full bg-gray-800 rounded-lg mt-5" />
-
-
-                                <div className="mt-4 sm:mt-6 md:mt-10 flex flex-col xs:flex-row justify-between gap-3 md:gap-1 w-full">
-                                    <div className="hidden sm:flex gap-2 items-center">
-                                        <p className="text-white flex gap-2 items-center text-sm sm:text-base">
-                                            <span><Timer className="w-4 h-4 sm:w-5 sm:h-5 lg:w-6 lg:h-6" /></span>
-                                            Time Period
-                                            <span className="text-cyan-500">{time}</span>
-                                        </p>
+                                    <div className="text-white w-full">
+                                        <SplitText
+                                            text={title}
+                                            className="text-base sm:text-lg md:text-2xl w-full font-semibold"
+                                            delay={500} duration={1.9} ease="power3.out"
+                                            splitType="words"
+                                            from={{ opacity: 0, y: 40 }} to={{ opacity: 1, y: 0 }}
+                                            threshold={0.1} rootMargin="-100px" showCallback={false}
+                                        />
                                     </div>
 
-                                    <div className="flex gap-1">
-                                        <button className="preview-btn px-2 sm:px-5 py-2.5 bg-transparent hover:shadow-[0_0_20px_rgba(255,255,255,0.1)] w-full xs:w-auto">
-                                            <span className="flex items-center justify-center gap-2 text-sm sm:text-base">
-                                                <FaGithub className="w-4 h-4 sm:w-5 sm:h-5" />
-                                                GitHub
-                                            </span>
-                                        </button>
+                                    <div className="hidden sm:block h-px w-full bg-gray-600 rounded-lg mt-3" />
 
-                                        <button className="preview-btn px-2 sm:px-5 py-2.5 bg-transparent hover:shadow-[0_0_20px_rgba(255,255,255,0.1)] w-full xs:w-auto"
-
-                                            onClick={() => {
-                                                const actions = {
-                                                    1: () => window.open("https://cashflow-expensetracker.netlify.app/", "_blank"),
-                                                    2: () => onButtonClick(),
-                                                    3: () => onButtonClick(),
-                                                };
-                                                actions[id]?.();
-                                            }}
-                                        >
-                                            <span className="flex items-center justify-center gap-2 text-sm sm:text-base">
-                                                <SquareArrowOutUpRight className="w-4 h-4 sm:w-5 sm:h-5" />
-                                                Preview
-                                            </span>
-                                        </button>
+                                    <div className="hidden sm:block w-full mt-4 md:mt-5 text-gray-400">
+                                        <SplitText
+                                            text={description}
+                                            className="text-sm md:text-base font-semibold"
+                                            delay={10} duration={1.9} ease="power3.out"
+                                            splitType="words"
+                                            from={{ opacity: 0, y: 40 }} to={{ opacity: 1, y: 0 }}
+                                            threshold={0.1} rootMargin="-100px" showCallback={false}
+                                        />
                                     </div>
-                                </div>
 
+                                    <div className="grid grid-cols-4 lg:grid-cols-4 gap-2 sm:gap-3 mt-3 sm:mt-5 w-full">
+                                        {techstack.map((stacks, index) => (
+                                            <button
+                                                key={index}
+                                                className="flex items-center justify-center gap-1.5 py-2 rounded-md bg-white/5 backdrop-blur-md text-white text-[8px] sm:text-xs lg:text-sm transition-all duration-300 hover:bg-white/10 hover:border-white/40 hover:scale-[1.03] active:scale-[0.97]"
+                                            >
+                                                <span>{stacks.logo}</span>
+                                                {stacks.name}
+                                            </button>
+                                        ))}
+                                    </div>
+
+                                    <div className="hidden sm:block h-px w-full bg-gray-800 rounded-lg mt-5" />
+
+
+                                    <div className="mt-4 sm:mt-6 md:mt-10 flex flex-col xs:flex-row justify-between gap-3 md:gap-1 w-full">
+                                        <div className="hidden sm:flex gap-2 items-center">
+                                            <p className="text-white flex gap-2 items-center text-sm sm:text-base">
+                                                <span><Timer className="w-4 h-4 sm:w-5 sm:h-5 lg:w-6 lg:h-6" /></span>
+                                                Time Period
+                                                <span className="text-cyan-500">{time}</span>
+                                            </p>
+                                        </div>
+
+                                        <div className="flex gap-1">
+
+                                            <button className="preview-btn px-2 sm:px-5 py-2.5 bg-transparent hover:shadow-[0_0_20px_rgba(255,255,255,0.1)] w-full xs:w-auto"
+                                                onClick={() => {
+                                                    const actions = {
+                                                        1: () => window.open("https://github.com/Soumya1974/CashFlow", "_blank"),
+                                                        2: () => window.open("https://github.com/Soumya1974/Animation-lib", "_blank"),
+                                                        3: () => onButtonClick(),
+                                                    };
+                                                    actions[id]?.();
+                                                }}
+                                            >
+                                                <span className="flex items-center justify-center gap-2 text-sm sm:text-base">
+                                                    <FaGithub className="w-4 h-4 sm:w-5 sm:h-5" />
+                                                    GitHub
+                                                </span>
+                                            </button>
+
+                                            <button className="preview-btn px-2 sm:px-5 py-2.5 bg-transparent hover:shadow-[0_0_20px_rgba(255,255,255,0.1)] w-full xs:w-auto"
+
+                                                onClick={() => {
+                                                    const actions = {
+                                                        1: () => window.open("https://cashflow-expensetracker.netlify.app/", "_blank"),
+                                                        2: () => onButtonClick(),
+                                                        3: () => onButtonClick(),
+                                                    };
+                                                    actions[id]?.();
+                                                }}
+                                            >
+                                                <span className="flex items-center justify-center gap-2 text-sm sm:text-base">
+                                                    <SquareArrowOutUpRight className="w-4 h-4 sm:w-5 sm:h-5" />
+                                                    Preview
+                                                </span>
+                                            </button>
+                                        </div>
+                                    </div>
+
+                                </div>
                             </div>
                         </div>
-                    </div>
-                );
-            })}
+                    );
+                })}
+            </div>
         </div>
     )
 }
